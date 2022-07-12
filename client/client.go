394873c9cd366a7610be71c5084b979b10ce326d
Ezequiel Pilcsuk da Rosa <34169938+ezequielpilcsuk@users.noreply.github.com>
2022-07-12 19:48:40 -0300

    Update client.go

--- client/client.go
+++ client/client.go
@@ -17,15 +17,17 @@ func main() {
 
 	log.Println("dealer created and connected")
 
-	msg := ""
-	if len(os.Args) <= 2 {
-		fmt.Printf("all valid requests use at least 3 arguments:\n")
-	} else {
-		for i := 1; i < len(os.Args); i++ {
-			msg += fmt.Sprintf("%v ", os.Args[i])
-		}
-		msg = msg[:len(msg)-1]
+	if len(os.Args) < 4 {
+		fmt.Printf("invalid request, all valid requests use at least 3 arguments:\n")
+		//TODO: could ask for user input in case not enough arguments
+		return
 	}
+
+	msg := ""
+	for i := 1; i < len(os.Args); i++ {
+		msg += fmt.Sprintf("%v ", os.Args[i])
+	}
+	msg = msg[:len(msg)-1]
 
 	err = dealer.SendFrame([]byte(msg), goczmq.FlagNone)
 	if err != nil {
